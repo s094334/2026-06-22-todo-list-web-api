959ef8cdea5f77c22f9164fb930d0e056a01c014
Chelsea <s094334@gmail.com>
2026-06-22 17:12:24 +0800

    fix: sign up typo

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const baseUrl = 'https://todolist-api.hexschool.io';
 
+// 註冊帳號
 const signUpEmail = document.querySelector('#sign-up-email');
 const nickName = document.querySelector('#name');
-const signUppwd = document.querySelector('#sign-up-pwd');
-const signUppwdAgain = document.querySelector('#sign-up-pwd2');
-const signUpBtn = document.querySelector('.sign-up-btn')
+const signUpPwd = document.querySelector('#sign-up-pwd');
+const signUpPwdAgain = document.querySelector('#sign-up-pwd2');
+const signUpBtn = document.querySelector('.sign-up-btn');
 
 signUpBtn.addEventListener("click", function(e) {
-  if (signUpEmail.value.trim() === '' || nickName.value.trim() === '' || signUppwd.value.trim() === '' || signUppwdAgain.value.trim() === '') {
+  if (signUpEmail.value.trim() === '' || nickName.value.trim() === '' || signUpPwd.value.trim() === '' || signUpPwdAgain.value.trim() === '') {
     alert("不能輸入空白值");
     return
   }
-  if (signUppwd.value.trim().length <= 6) {
+  if (signUpPwd.value.trim().length <= 6) {
     alert("password 長度不足 6 個字");
     return
   }
-  if (signUppwd.value !== signUppwdAgain.value) {
+  if (signUpPwd.value !== signUpPwdAgain.value) {
     alert("再次輸入的密碼不符");
     return
   }
-  signUp(signUpEmail.value, signUppwd.value, nickName.value)
+  signUp(signUpEmail.value, signUpPwd.value, nickName.value);
 })
 
-async function signUp(signUpEmail, signUppwd, nickName) {
+async function signUp(signUpEmail, signUpPwd, nickName) {
   try {
     const response = await fetch(`${baseUrl}/users/sign_up`,
       {
@@ -30,7 +31,7 @@ async function signUp(signUpEmail, signUppwd, nickName) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           "email": signUpEmail,
-          "password": signUppwd,
+          "password": signUpPwd,
           "nickname": nickName
         })
       }
